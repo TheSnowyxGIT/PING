090c10e8a5e0287a2bb8843fe4d3efd01678f68c
romain.nakusi <romain.nakusi@gmail.com>
2022-06-27 22:33:37 +0200

    uname

--- src/classes/FileNode.ts
+++ src/classes/FileNode.ts
@@ -6,33 +6,30 @@ import { Report } from "../shared/report";
 
 export class FileNode implements F_Node{
   public path: string;
+  public relativePath: string;
   public type: NodeType;
   public children: FileNode[];
   public name: string;
 
   public static of(node: F_Node): FileNode{
     return node as FileNode;
-}
+  }
 
-  constructor(path: string, name: string, type: NodeType, children: FileNode[]) {
+  constructor(path: string, relativePath: string, name: string, type: NodeType, children: FileNode[]) {
     this.path = path;
+    this.relativePath = relativePath;
     this.name = name;
     this.type = type;
     this.children = children;
 
   }
   
-  public getPath(hardPath: string, rootPath: string): string {
-    let path : string= hardPath.substring(hardPath.indexOf(rootPath) + rootPath.length);
-    return this.path; 
-  }
-  // add node to tree
-
   public getChild():FileNode
   {
     
   }
-
+  
+  // add node to tree
   public addNode(pah:string, node: F_Node): boolean {
     
     
